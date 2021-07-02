d83470fd3eb19295af20166aeb38e835906fb811
Citlali Orduño <65271029+CitOrd@users.noreply.github.com>
2021-07-02 03:06:20 -0600

    pantallas again diosito ayuda

---
+++ app/src/main/java/aldana/jesus/thedot/create_account.kt
@@ -0,0 +1,11 @@
+package aldana.jesus.thedot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create_account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
---
+++ app/src/main/java/aldana/jesus/thedot/sign_in.kt
@@ -0,0 +1,11 @@
+package aldana.jesus.thedot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sign_in : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
