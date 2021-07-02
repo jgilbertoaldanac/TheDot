59ccd52f35d56867d899a33696e15a49b46979e9
Gilberto Aldana <78342937+jgilbertoaldanac@users.noreply.github.com>
2021-07-02 03:15:35 -0700

    Login e Inicio
    Cambios en Login, Inicio, Strings y Colors

--- app/src/main/java/aldana/jesus/thedot/MainActivity.kt
+++ app/src/main/java/aldana/jesus/thedot/MainActivity.kt
@@ -1,11 +1,20 @@
 package aldana.jesus.thedot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.Inicio)
+        setContentView(R.layout.inicio)
+
+        val buttonIniciarSesion: Button = findViewById(R.id.btn_iniciaSesion) as Button
+
+        buttonIniciarSesion.setOnClickListener{
+            var intent: Intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
