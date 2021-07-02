0aeea049067f384d08fd8255a62eb7d57d423ff7
Citlali Orduño <65271029+CitOrd@users.noreply.github.com>
2021-07-02 02:59:42 -0600

    subpantallas creadas

---
+++ app/src/main/java/aldana/jesus/thedot/howareyoutoday.kt
@@ -0,0 +1,11 @@
+package aldana.jesus.thedot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class howareyoutoday : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_howareyoutoday)
+    }
+}
---
+++ app/src/main/java/aldana/jesus/thedot/intentions_day.kt
@@ -0,0 +1,11 @@
+package aldana.jesus.thedot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class intentions_day : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intentions_day)
+    }
+}
