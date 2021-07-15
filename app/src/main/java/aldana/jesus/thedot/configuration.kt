ac603b33930aa66e0e37f7841f6f801213cd861f
Gilberto Aldana <78342937+jgilbertoaldanac@users.noreply.github.com>
2021-07-15 14:34:37 -0700

    Update configuration.kt

--- app/src/main/java/aldana/jesus/thedot/configuration.kt
+++ app/src/main/java/aldana/jesus/thedot/configuration.kt
@@ -13,7 +13,9 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class configuration : AppCompatActivity() {
-    private val db= FirebaseFirestore.getInstance()
+    //private val db= FirebaseFirestore.getInstance()
+    private lateinit var storage: FirebaseFirestore
+    private lateinit var usuario: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +29,51 @@ class configuration : AppCompatActivity() {
         var et_ocupacion: EditText = findViewById(R.id.et_ocupacion) as EditText
         var et_contrasenia: EditText = findViewById(R.id.et_contrasenia) as EditText
 
+        storage = FirebaseFirestore.getInstance()
+        usuario = FirebaseAuth.getInstance()
+
+        var nombre: String = ""
+        var fechaNac: String = ""
+        var ubicacion: String = ""
+        var ocupacion: String = ""
+        var contrasenia: String = ""
+
+        storage.collection("usuarios").whereEqualTo("correo",usuario.currentUser?.email).get().addOnSuccessListener {
+            it.forEach {
+                nombre = it.getString("nombre").toString()
+                fechaNac = it.getString("fechaNac").toString()
+                ubicacion = it.getString("ubicacion").toString()
+                ocupacion = it.getString("ocupacion").toString()
+                contrasenia = it.getString("contrasenia").toString()
+                }
+            }
+
+        if(!nombre.isNullOrBlank()){
+            et_nombre.setText(nombre)
+        }
+        if(!fechaNac.isNullOrBlank()){
+            et_fechaNac.setText(fechaNac)
+        }
+        if(!ubicacion.isNullOrBlank()){
+            et_ubicacion.setText(ubicacion)
+        }
+        if(!ocupacion.isNullOrBlank()){
+            et_ocupacion.setText(ocupacion)
+        }
+        if(!contrasenia.isNullOrBlank()){
+            et_contrasenia.setText(contrasenia)
+        }
+
+        val data = hashMapOf(
+            "nombre" to nombre,
+            "fechaNac" to fechaNac,
+            "ubicacion" to ubicacion,
+            "ocupacion" to ocupacion,
+            "contrasenia" to contrasenia
+        )
+
         btn_close.setOnClickListener{
+            storage.collection("usuarios").document(usuario.currentUser?.email.toString()).set(data)
             var intent: Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
